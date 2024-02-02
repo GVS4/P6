5120b44d6188f078ff8020d0042215b02da93954
GVS <johndoe@exemple.com>
2024-02-02 18:24:36 +0100

    E1 done

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,61 +1,17 @@
-// -------------------------------CODE DE SAGOUIN---------------------------------
+// Fetches works data from the API
+const fetchData = async param => {
+  try {
+    const response = await fetch(`http://localhost:5678/api/${param}`);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-
-// Return works
-// async function getWorks() {
-//   return (await fetch("http://localhost:5678/api/works")).json();
-// }
-
-// Affichage des works
-// async function displayWorks() {
-//   const arrayWorks = await getWorks();
-//   const galleryElement = document.querySelector(".gallery");
-//   galleryElement.innerHTML = "";
-//   arrayWorks.forEach((work) => {
-
-//     const figureElement = document.createElement("figure");
-//     const imgElement = document.createElement("img");
-//     const figcaptionElement = document.createElement("figcaption");
-//     imgElement.src = work.imageUrl;
-//     figcaptionElement.textContent = work.title;
-
-//     galleryElement.appendChild(figureElement);
-//       figureElement.appendChild(imgElement);
-//       figureElement.appendChild(figcaptionElement);
-//   });
-// }
-// displayWorks();
-
-
-// async function getCategorys(params) {
-//   const response = await fetch("http://localhost:5678/api/categories");
-//   return await response.json();
-// }
-
-// async function displayCategorysButtons() {
-//   const categorys = await getCategorys();
-//   categorys.forEach(category => {
-//     const btn = document.createElement("button")
-//     btn.textContent = category.name;
-//     btn.id = category.id
-//     btn.classList.add("button")
-//     const filterElement = document.getElementById("filter")
-//     filterElement.appendChild(btn)
-//   });
-// }
-// displayCategorysButtons()
-
-// -------------------------------CODE Propre ---------------------------------
-
-
-// request HTTP
-const getWorks = async () =>
-  (await fetch("http://localhost:5678/api/works")).json();
-
-// Display works
-async function displayWorks() {
-  const arrayWorks = await getWorks();
-  document.querySelector(".gallery").innerHTML = arrayWorks
+// Inserts works into the HTML gallery
+const insertWorkInHtml = async (array) => {
+  document.getElementById("gallery").innerHTML = array
     .map(
       (e) =>
         `<figure>
@@ -64,19 +20,33 @@ async function displayWorks() {
         </figure>`
     )
     .join("");
-}
+};
+
+// Displays all works in the HTML gallery
+const displayWorks = async () => insertWorkInHtml(await fetchData("works"));
 displayWorks();
 
-// Request HTTP
-const getCategories = async () =>
-  (await fetch("http://localhost:5678/api/categories")).json();
-
-// DisplayBtnCategories
+// Displays category buttons and adds "All" as the first option
 const DisplayBtnCategories = async () => {
-  const ArraysCategories = await getCategories();
-  ArraysCategories.unshift({ id: 0, name: "Tous" });
-  document.getElementById("filter").innerHTML = ArraysCategories.map(
-    (e) => `<button class="button" id ="${e.id}">${e.name}</button>`
-  ).join("");
+  const dataArray = await fetchData("categories");
+  dataArray.unshift({ id: 0, name: "Tous" });
+  document.getElementById("filter").innerHTML = dataArray
+    .map((e) => `<button class="button" id ="${e.id}">${e.name}</button>`)
+    .join("");
 };
 DisplayBtnCategories();
+
+// Filters works with a click on btn and updates the HTML gallery
+async function filterWorks() {
+  const dataArray = await fetchData("works");
+  const arrayBtn = Array.from(document.querySelectorAll("#filter button"));
+
+  arrayBtn.forEach((btn) =>
+    btn.addEventListener("click", async () => {
+      const btnId = +btn.id;
+      const workFiltered = btnId === 0 ? dataArray : dataArray.filter((e) => btnId === e.categoryId);
+      insertWorkInHtml(workFiltered);
+    })
+  );
+}
+filterWorks();
